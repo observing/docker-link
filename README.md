647152919a5d92421b2f9e5696a4aa9af6e2f2dd
Swaagie <info@martijnswaagman.nl>
2014-09-26 13:01:05 +0200

    [major] initial working module

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
-docker-link
-===========
+# docker-link
 
-Parser for reading environment variables set by docker link
+Parser for reading environment variables set by docker link. Reading the
+environment variables available to the `CMD` running inside a docker container
+can be a bit cumbersome as it depends on the `alias` chosen to name the link.
+This can be easily fixed to prepend variables with prefix of your choosing.
+
+### Installation
+
+```bash
+npm install --save docker-link
+```
+
+### Usage
+
+Suppose the following environment variables were set by creating a link
+between your application and a Docker container running Redis. Those Docker
+link variables can then be read from the application by using `new Link`.
+
+```bash
+PREFIX_REDIS_PORT=tcp://172.17.0.2:6379
+PREFIX_REDIS_PORT_6379_TCP=tcp://172.17.0.2:6379
+PREFIX_REDIS_PORT_6379_TCP_ADDR=172.17.0.2
+PREFIX_REDIS_PORT_6379_TCP_PORT=6379
+PREFIX_REDIS_PORT_6379_TCP_PROTO=tcp
+PREFIX_REDIS_NAME=/conductor/db
+```
+
+```js
+var Link = require('docker-link')
+  , link = new Link({ prefix: 'PREFIX_' });
+
+//
+// Which would result in: link.get('REDIS');
+//
+{
+  PORT_TCP: 'tcp://172.17.0.2:6379',
+  PORT: 'tcp://172.17.0.2:6379',
+  PORT_TCP_ADDR: '172.17.0.2',
+  PORT_TCP_PORT: '6379',
+  PORT_TCP_PROTO: 'tcp',
+  NAME: '/container/redis'
+}
+```
---
+++ index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+/**
+ * Constructor for Docker Link helper module.
+ *
+ * @Constructor
+ * @param {Object} options
+ * @api public
+ */
+function Link(options) {
+  options = options || {};
+
+  //
+  // Prepare empty data object and set prefix.
+  //
+  this.prefix = (options.prefix || 'OBS_').toUpperCase();
+  this.data = {};
+
+  this.read();
+};
+
+/**
+ * Read prefixed environment variables and parse. This will result in a hash
+ * stored in `this.data` per link alias (without the prefix).
+ *
+ * @returns {Link} Fluent interface
+ * @api public
+ */
+Link.prototype.read = function read() {
+  for (var key in process.env) {
+    if (~key.indexOf(this.prefix)) this.parse(key, process.env[key]);
+  }
+
+  return this;
+}
+
+/**
+ * Parse all the known environment variables created by Docker.
+ *
+ * @param {String} key Enviroment variable name
+ * @param {Mixed} value Environment variable value
+ * @returns {Link} Fluent interface
+ * @api private
+ */
+Link.prototype.parse = function(key, value) {
+  var keys = key.substring(this.prefix.length).split('_')
+    , directive = keys.slice(1);
+
+  //
+  // Only allow a specific subset of keys.
+  //
+  if (!/^NAME$|^PORT$|^PORT_\d+_TCP(_ADDR|_PORT|_PROTO)?$/.test(
+    directive.join('_')
+  )) return this;
+
+  //
+  // Prepare the alias group.
+  //
+  if (!(keys[0] in this.data)) this.data[keys[0]] = {};
+
+  //
+  // Store the value without the port as that is usally variable
+  // and hard to reference properly from scripts.
+  //
+  directive.splice(1, 1);
+  this.data[keys[0]][directive.join('_')] = value;
+
+  return this;
+}
+
+/**
+ * Get the hash from the stored data.
+ *
+ * @param {String} key Docker alias without the prefix.
+ * @return {Object} Hash with environment values.
+ * @api public
+ */
+Link.prototype.get = function(key) {
+  return this.data[key.toUpperCase()];
+}
+
+//
+// Expose the constructor
+//
+exports.module = Link;
